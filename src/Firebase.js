00abf5769a779180f3f45899eb6b836d242c6d29
Connor Carson <connordcarson@Connors-MacBook-Pro.local>
2020-10-27 12:07:47 -0500

    first commit

---
+++ src/Firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDbPtFNQN811mEGYV7b1Dc2aTlO-VsrLj0",
+    authDomain: "foodcomputer-7d1a2.firebaseapp.com",
+    databaseURL: "https://foodcomputer-7d1a2.firebaseio.com",
+    projectId: "foodcomputer-7d1a2",
+    storageBucket: "foodcomputer-7d1a2.appspot.com",
+    messagingSenderId: "796069184170",
+    appId: "1:796069184170:web:26431fc8673f25a615c6d3",
+    measurementId: "G-YF2CSYBVNE"
+  };
